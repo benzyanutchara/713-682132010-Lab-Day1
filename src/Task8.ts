9c86faf0fefdad241d842a98894ccb2b58360da7
benzyanutchara <yanutchara_kan@cmu.ac.th>
2025-11-29 17:13:32 +0700

    task8.1

---
+++ src/Task8.ts
@@ -0,0 +1,22 @@
+// @ts-ignore
+
+let w:number = 7
+if (w > 10) {
+    console.log('w is more than 10')
+}else {
+    console.log('w is less than 10')
+}
+
+let p:number = 15
+if (p > 10) {
+    console.log('p is more than 10')
+}else {
+    console.log('p is less than 10')
+}
+
+let x: number = 4
+let y = 50
+if (x > 5 && y > 12)
+    console.log('x is more than 5 and y is more than 12')
+else
+    console.log(x*y + x/y)
