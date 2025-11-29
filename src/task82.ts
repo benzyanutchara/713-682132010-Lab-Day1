3bfb41579b965b59fff19d7c20c557bba2317034
benzyanutchara <yanutchara_kan@cmu.ac.th>
2025-11-29 17:25:49 +0700

    task8.2

---
+++ src/task82.ts
@@ -0,0 +1,29 @@
+let x =7
+if (x > 10) {
+    console.log('x is more than 10')
+} else if (x < 5){
+    console.log('x is less than 5')
+}
+else {
+    console.log('x is between 5 and 10')
+}
+
+let c = 2
+if (c > 10) {
+    console.log('c is more than 10')
+} else if (c < 5){
+    console.log('c is less than 5')
+}
+else {
+    console.log('c is between 5 and 10')
+}
+
+let v = 11
+if (v > 10) {
+    console.log('v is more than 10')
+} else if (v < 5){
+    console.log('v is less than 5')
+}
+else {
+    console.log('v is between 5 and 10')
+}
