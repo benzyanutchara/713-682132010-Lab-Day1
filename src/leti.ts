b8816145985f83a24cd2978e935d60e5467ccbab
benzyanutchara <yanutchara_kan@cmu.ac.th>
2025-11-29 16:37:39 +0700

    Task 7

---
+++ src/leti.ts
@@ -0,0 +1,17 @@
+interface Person {
+    name: string;
+    age: number;
+}
+
+
+const a: Person = {
+    name: 'Alice',
+    age: 30
+}
+
+a.name = 'Benz'
+a.age = 28
+console.log(a)
+
+const list = [6, 7, 8, 9, 10];
+console.log(list);
