9d99b18f0be9bffe35db225c6117735a24897c36
benzyanutchara <yanutchara_kan@cmu.ac.th>
2025-12-05 15:33:18 +0700

    task8.3

---
+++ src/task83.ts
@@ -0,0 +1,12 @@
+let score = 80
+if (score >= 80) {
+    console.log('You got an A')
+} else if (score >= 60){
+    console.log('You got a B')
+}
+else if (score >= 50){
+    console.log('You got a C')
+}
+else if (score <= 49){
+    console.log('You got a F')
+}
